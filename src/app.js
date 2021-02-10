4359841703c20f032acde5800618c280cc4d717a
Irving Caamal <irvv.17@gmail.com>
2021-02-10 00:32:53 -0500

    [WIP] modal

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
+'use strict'
 
-window.addEventListener('click', '', '');
+function toggleModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = 'flex'
+}
+
+function clickButton() {
+console.log('click')
+    toggleModal()
+}
+
+function render () {
+    const showMore = document.getElementById('show-more')
+
+    console.log(showMore)
+    showMore.addEventListener('click', () => {
+        console.log('click')
+    });
+
+    console.log('render')
+}
+
+render()
