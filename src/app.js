48065286e81635d5316c1bfdf9d33cc8bb69db5a
Irving <irvv.17@gmail.com>
2021-02-12 00:05:16 -0500

    Dev/irvv17 (#5)
    * [ADD] animations for skills
    
    * [WIP] modal styles
    
    * [WIP] modal
    
    * [ADD]➕  Github Templates

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function toggleModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = 'flex'
+}
+
+function clickButton() {
+console.log('click')
+    toggleModal()
+}
+
+const showMoreButton = document.querySelector('#show-more').addEventListener('click', function() {
+    console.log('click')
+});
