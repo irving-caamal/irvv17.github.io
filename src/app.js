aaae4c9020ef0cb886917db3d0c4902b3b583510
Irving Caamal <irvv.17@gmail.com>
2021-02-13 00:06:50 -0500

    [ADD] Modal reusable and global

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
+'use strict'
 
 function toggleModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = 'flex'
+    const modal = document.querySelector(".modal");
+    console.log(modal.style['display'])
+    if (modal.style.display === 'block') {
+       return modal.style.display = 'none'
+    }
+   return modal.style.display = 'block'
 }
 
-function clickButton() {
-console.log('click')
-    toggleModal()
+function renderActions () {
+    const button = document.querySelector('#show-more');
+    document.querySelectorAll('.carousel-item').forEach(skill =>{
+        return   skill.addEventListener('click', function() {
+            console.log('skill')
+            toggleModal();
+        });
+    });
+    const close = document.querySelector('#close-modal-button');
+
+    button.addEventListener('click', function() {
+        toggleModal();
+    });
+
+    close.addEventListener('click', function() {
+        toggleModal();
+    });
 }
 
-const showMoreButton = document.querySelector('#show-more').addEventListener('click', function() {
-    console.log('click')
-});
+renderActions()
