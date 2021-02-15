fe16f7e593701df3340a6ead1b89da9f97986023
Irving Caamal <irvv.17@gmail.com>
2021-02-14 22:50:24 -0500

    [ADD] Modal and JS interation

--- src/app.js
+++ src/app.js
@@ -1,35 +1,5 @@
 'use strict'
 
-function toggleModal(title = '') {
-    const modal = document.querySelector(".modal");
-    console.log(modal.style['display'])
-    const modalTitle = document.querySelector(".modal-title")
-    modalTitle.innerHTML = title;
-    if (modal.style.display === 'block') {
-       return modal.style.display = 'none'
-    }
-   return modal.style.display = 'block'
-}
-
-function renderActions () {
-    const button = document.querySelector('#show-more');
-    document.querySelectorAll('.carousel-item').forEach(skill =>{
-        return   skill.addEventListener('click', function() {
-            console.log('skill', )
-            toggleModal();
-        });
-    });
-    const close = document.querySelector('#close-modal-button');
-
-    button.addEventListener('click', function(e) {
-        console.log(e.data,button.dataset,'button')
-        const title = button.dataset.id;
-        toggleModal(title);
-    });
-
-    close.addEventListener('click', function() {
-        toggleModal();
-    });
-}
+import {renderActions} from "./utils/actions.js";
 
 renderActions()
