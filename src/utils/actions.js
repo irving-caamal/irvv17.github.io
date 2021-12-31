226543db4acd5343ec21c3508fd99380645a898d
irvv17 <irvv.17@gmail.com>
2021-12-31 00:39:15 -0500

    fix: style issues

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -7,10 +7,30 @@ function buttonsActions() {
 
     buttons.forEach(button => {
         let title = button.dataset.id;
-        return   button.addEventListener('click', function() {
+        button.addEventListener('click', function() {
             toggleModal(title);
         });
+        return;
     });
+
+    const form = document.getElementById( "contact-form" );
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const elements = this.elements
+        console.log({ elements })
+        console.log({ len: elements.length})
+        for(let i=0; i< elements.length; i++) {
+            if(['submit'].includes(elements[i].type)) {
+                alert('false')
+                return false;
+            }
+
+            let formData = new FormData(form);
+            console.log({ fd: formData})
+            console.log(elements[i].value)
+        }
+    });
+
 }
 function renderActions() {
     actionsSkills()
