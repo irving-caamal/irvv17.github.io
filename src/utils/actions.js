8fa7e4607ac9e405f0e6a59ee52bbd3bae3255ba
Irving <irvv.17@gmail.com>
2022-01-03 23:48:58 -0500

    fix: multiple style issues (#11)

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -3,14 +3,31 @@ import { actionsSkills } from "./actionsSkills.js";
 
 
 function buttonsActions() {
-    const buttons = document.querySelectorAll('.button');
+    const buttons = document.querySelectorAll('.button:not([type=submit])');
 
     buttons.forEach(button => {
         let title = button.dataset.id;
-        return   button.addEventListener('click', function() {
+        button.addEventListener('click', function() {
             toggleModal(title);
         });
+        return;
     });
+
+    const form = document.getElementById( "contact-form" );
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const elements = this.elements
+        for(let i=0; i< elements.length; i++) {
+            if(['submit'].includes(elements[i].type)) {
+                return false;
+            }
+
+            let formData = new FormData(form);
+            console.log({ fd: formData})
+            console.log(elements[i].value)
+        }
+    });
+
 }
 function renderActions() {
     actionsSkills()
